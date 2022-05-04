9e0c213426acfda540f32787392cc860886e402e
hdragon9191 <hassanbutgithub@gmail.com>
2022-05-04 19:10:33 +0300

    Revert "Door and sianna's model"
    This reverts commit fb091c2e51697b1e9e046636eb499f4a68e97d06.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,24 +4,15 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    Animator m_Animator;
-
     // Start is called before the first frame update
     void Start()
     {
-        m_Animator = gameObject.GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Player" && Input.GetButton("Interact"))
-        {
-            m_Animator.SetTrigger("open");
-        }
     }
 }
