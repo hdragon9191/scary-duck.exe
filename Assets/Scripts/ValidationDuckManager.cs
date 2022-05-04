5d76db76990672dfc0965b6cd96ef86e5b022b69
Vittorio CHIUSELLI <vittorio.chiuselli@gmail.com>
2022-05-04 23:31:25 +0200

    Implement score points and duck missed interface

--- Assets/Scripts/ValidationDuckManager.cs
+++ Assets/Scripts/ValidationDuckManager.cs
@@ -2,27 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ValidationDuckManager : MonoBehaviour {
 
     private int scorePoint;
     private int defectiveDuckCounter;
+    [SerializeField] TextMeshProUGUI defectiveDuckCounterText;
+    [SerializeField] TextMeshProUGUI scorePointText;
 
     void Start() {
         scorePoint = 0;
         defectiveDuckCounter = 0;
     }
 
-    // Update is called once per frame
-    void Update() {
-
-    }
-
     public void IncreaseScorePoint() {
         scorePoint++;
+        scorePointText.text = "Duck delivered: " + scorePoint;
     }
 
     public void IncreaseDefectiveDuckCounter() {
         defectiveDuckCounter++;
+        defectiveDuckCounterText.text = "Duck missed " + defectiveDuckCounter;
     }
 }
