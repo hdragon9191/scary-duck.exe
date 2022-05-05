789d54ca32a5196a60c1a07f32b885995112accb
Vittorio CHIUSELLI <vittorio.chiuselli@gmail.com>
2022-05-05 12:10:30 +0200

    When the game start the camera approach to audiospeaker

--- Assets/Scripts/TriggerEventJobDescription.cs
+++ Assets/Scripts/TriggerEventJobDescription.cs
@@ -1,18 +1,38 @@
+using StarterAssets;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerEventJobDescription : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class TriggerEventJobDescription : MonoBehaviour {
+    [SerializeField] FirstPersonController firstPersonController;
+    [SerializeField] Transform audioSpeakerPosition;
+    [SerializeField] Animator animator;
+    [SerializeField] GameObject cameraRoot;
+    GameObject cameraRootStarter = null;
+    bool triggerEventIsEnded;
+
+    void Start() {
+        cameraRootStarter = cameraRoot;
+        animator.enabled = false;
+        triggerEventIsEnded = false;
+        firstPersonController.enabled = false;
+        StartCoroutine(JobDescriptionTutorial());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    IEnumerator JobDescriptionTutorial() {
+        animator.enabled = true;
+        yield return new WaitForSeconds(3f);
+
+        triggerEventIsEnded = true;
+        animator.enabled = false;
     }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.E) && triggerEventIsEnded) {
+            firstPersonController.enabled = true;
+            animator.Rebind();
+        }
+    }
+
 }
