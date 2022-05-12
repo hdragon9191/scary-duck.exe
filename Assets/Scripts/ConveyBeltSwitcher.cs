86c5c8aab728c3e4b169a33dbca028c42addfe29
Vittorio CHIUSELLI <vittorio.chiuselli@gmail.com>
2022-05-12 11:40:32 +0200

    Add tooltip text to switch the power of conveyorbelt

--- Assets/Scripts/ConveyBeltSwitcher.cs
+++ Assets/Scripts/ConveyBeltSwitcher.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ConveyBeltSwitcher : MonoBehaviour {
     [SerializeField] ConveyorBeltActivator conveyorBeltActivator;
-
+    [SerializeField] GameObject tooltipText;
     void Update() {
         if (Input.GetKeyDown(KeyCode.E)) {
             conveyorBeltActivator.IsActive = !conveyorBeltActivator.IsActive;
             //TODO add noise of switch off
         }
     }
+
+    private void OnTriggerEnter(Collider other) {
+        tooltipText.SetActive(true);
+        tooltipText.GetComponent<TextMeshProUGUI>().text = "Press E to power On/Off";
+    }
+    private void OnTriggerExit(Collider other) {
+        tooltipText.SetActive(false);
+    }
+
 }
