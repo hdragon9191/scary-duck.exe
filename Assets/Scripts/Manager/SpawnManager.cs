e0a544b14f4b41e96d74b3f0b646476c7f09b6e5
Vittorio CHIUSELLI <vittorio.chiuselli@gmail.com>
2022-05-13 00:00:49 +0200

    Clean main scene and cap the min and max spawn rateo

--- Assets/Scripts/Manager/SpawnManager.cs
+++ Assets/Scripts/Manager/SpawnManager.cs
@@ -9,25 +9,35 @@ public class SpawnManager : MonoBehaviour {
     int children;
     [SerializeField] GameObject tooltipText;
     [SerializeField] GameObject spawnRatio;
+    public float MaxValueSpawn;
+    public float MinValueSpawn;
 
     private void Start() {
+        MaxValueSpawn = 2f;
+        MinValueSpawn = 0.3f;
         AmountTimeSpawn = 1f;
         children = transform.childCount;
     }
 
     private void Update() {
-        spawnRatio.GetComponent<TextMeshPro>().text = "" +Math.Round(AmountTimeSpawn,2); //dump
+        spawnRatio.GetComponent<TextMeshPro>().text = "" + Math.Round(AmountTimeSpawn, 2); //dump
     }
 
     private void OnTriggerStay(Collider other) {
         //TODO manage a cap for min and max
         if (Input.GetKeyDown(KeyCode.DownArrow) && other.tag == "Player") {
             AmountTimeSpawn = (AmountTimeSpawn - 0.1f);
+            if (AmountTimeSpawn <= MinValueSpawn) {
+                AmountTimeSpawn = MinValueSpawn;
+            }
             CalibrateRatioSpawn(AmountTimeSpawn);
             //TODO add noise of switch off
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow) && other.tag == "Player") {
             AmountTimeSpawn = (AmountTimeSpawn + 0.1f);
+            if (AmountTimeSpawn >= MaxValueSpawn) {
+                AmountTimeSpawn = MaxValueSpawn;
+            }
             CalibrateRatioSpawn(AmountTimeSpawn);
             //TODO add noise of switch off
         }
