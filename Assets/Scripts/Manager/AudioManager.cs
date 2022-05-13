9674b3f72a43ae4b888de8ef689dc127a430cac6
Vittorio CHIUSELLI <vittorio.chiuselli@gmail.com>
2022-05-13 15:42:21 +0200

    Audio manager is persistent through the scenes

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -2,25 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
-{
+public class AudioManager : MonoBehaviour {
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip openLockerClip;
     [SerializeField] AudioClip buttonClip;
     [SerializeField] AudioClip conveyorOn;
     [SerializeField] AudioClip conveyorOff;
 
+    void Awake() {
+        int numGameSessions = FindObjectsOfType<AudioManager>().Length;
+        if (numGameSessions > 1) {
+            Destroy(gameObject);
+        }
+        else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     public void OpenDoorClip() {
         audioSource.PlayOneShot(openLockerClip);
     }
-    
+
     public void ButtonClip() {
         audioSource.PlayOneShot(buttonClip);
     }
-    
+
     public void ConveyorOnClip() {
         audioSource.PlayOneShot(conveyorOn);
-    } 
+    }
     public void ConveyorOffClip() {
         audioSource.PlayOneShot(conveyorOff);
     }
